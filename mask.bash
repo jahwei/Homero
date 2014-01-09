fef23b5b559df3559d9409eb2b178172157fc7c2
Jiawei He <jiawei@hydra.hydro.washington.edu>
2014-01-08 16:01:26 -0800

    Refined the comments

--- mask.bash
+++ mask.bash
@@ -2,31 +2,45 @@
 
 #The scripts aim to create CRU dataset from the mask file obtained from Maurer's dataset.
 
-#fn1 is one dataset from Maurer's, fn2 is the mask file made according to one of Maurer's dataset;fn3 is the mask file by averaging variables over time; fn4 is the CRU dataset in a global scale; fn5 is the dataset clipped from global dataset by setting the range of latitude and longitude; fn6 is the final dataset by working on clipped dataset and mask file from Maurer dataset. 
-
+#path to each dataset 
 idir=/raid2/homefc/conus/data.sets/Maurer
 odir=/raid3/jiawei/homero/cru/mask/Prec
 idir2=/raid2/homefc/conus/data.sets/CRU
+
+#one of Maurer's dataset used to create a mask file
 fn1=`printf "gridded_obs.monthly.Prcp.1991.nc"`
-fn2=`printf "test.nc"`
-fn3=`printf "mask_pre.nc"`
-fn4=`printf "cru_ts3.21.1901.2012.pre.dat.nc"`
-fn5=`printf "cru_ts3.21.1901.2012.pre.clip.nc"`
-fn6=`printf "cru_ts3.21.1901.2012.pre.mask.nc"`
+
+#mask file created from the dataset above
+fn2=`printf "test.nc"` 
+
+#mask file of precipitation by averaging preci over time
+fn3=`printf "mask_pre.nc"` 
+
+#CRU dataset in a global sca
+fn4=`printf "cru_ts3.21.1901.2012.pre.dat.nc"` 
+
+#dataset clipped from global dataset
+fn5=`printf "cru_ts3.21.1901.2012.pre.clip.nc"` 
+
+#targeted dataset derived from clipped dataset and mask file of Maurer dataset
+fn6=`printf "cru_ts3.21.1901.2012.pre.mask.nc"` 
 
 #clip parts of global map
-#I don't know why the range of lat,lon in the final dataset doesn't match the number as shown below. 
+#considering the grid of original dataset, the domain will probably not be exactly specified.
 ncks -d lat,25.1875,52.8125 -d lon,-124.688,-67.0625 $idir2/$fn4 $odir/$fn5
 
-#remap the grid of Maurer
+#remap the grid of Maurer to that of CRU
+#<operator>,ifile1 ifile2 ofile
 cdo remapcon,$odir/$fn5 $idir/$fn1 $odir/$fn1
 
-#create a mask file according to one of Maurer's dataset
+#create a mask file based on one of Maurer's dataset
+#<operator>,c ifile ofile
 cdo gtc,0.0 $odir/$fn1 $odir/$fn2
 
-#Average precipitation of mask file over time 
+#Eliminate time dimension by averaging precipitation of mask file over time 
 ncwa -O -v Prcp -a time $odir/$fn2 $odir/$fn3
 
 #create a dataset of CRU based on the mask file of Maurer's
+#<operator> ifile1 ifile2 ofile
 cdo ifthen $odir/$fn3 $odir/$fn5 $odir/$fn6
 
